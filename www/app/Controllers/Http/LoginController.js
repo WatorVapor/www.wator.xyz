6db8b7f1b1051b4d550e60038d5311c538fe34a0
Wator Vapor <watovapor@gmail.com>
2020-03-19 16:22:12 +0000

    adonis web

--- www/app/Controllers/Http/LoginController.js
+++ www/app/Controllers/Http/LoginController.js
@@ -1,9 +1,13 @@
 'use strict'
 
+const nacl = require('tweetnacl');
+nacl.util = require('tweetnacl-util');
+
 class LoginController {
   index ({ request, response,view  }) {
-    response.header('LoginRadom', 'application/json');
-    return view.render('login',{});
+    const random = nacl.randomBytes(32);
+    const randomBase64 = nacl.util.encodeBase64(random);
+    return view.render('login',{login_server_random:randomBase64});
   }
 }
 
