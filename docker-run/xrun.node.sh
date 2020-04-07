7f74db3c28eef78589d171837fdc7d8ef9a7e5ed
Wator Vapor <watovapor@gmail.com>
2020-04-07 13:05:04 +0000

    new app

--- docker-run/xrun.node.sh
+++ docker-run/xrun.node.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker run -it  node:lts /bin/bash
-
+docker run -it \
+  -v /watorvapor/workspace/www.wator.xyz/www2:/opt/www \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -u $(id -u $USER):$(id -g $USER) \
+  --workdir="/opt/www" \
+  node:lts /bin/bash
