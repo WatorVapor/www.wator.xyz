c7ab97e2ef46bd07311e76feb99ff29d58cedb13
Wator Vapor <watovapor@gmail.com>
2020-04-07 13:19:11 +0000

    ui

--- docker-run/xrun.node.sh
+++ docker-run/xrun.node.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 docker run -it \
-  -v /watorvapor/workspace/www.wator.xyz/www2:/opt/www \
+  -v /watorvapor/workspace/www.wator.xyz/adonis:/opt/www \
   -v /etc/passwd:/etc/passwd:ro \
   -v /etc/group:/etc/group:ro \
+  -v /watorvapor/workspace/www.wator.xyz/adonis/home:/home/$USER \
   -u $(id -u $USER):$(id -g $USER) \
   --workdir="/opt/www" \
   node:lts /bin/bash
--- www/app/Controllers/Http/HomeController.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-class HomeController {
-  index ({ request, response,view  }) {
-    return view.render('home',{});
-  }
-}
-module.exports = HomeController
