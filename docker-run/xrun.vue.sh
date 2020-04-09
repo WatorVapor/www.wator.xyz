c6200f1c9141a841ceb2e35fa37a1c81261a8121
Wator Vapor <watovapor@gmail.com>
2020-04-09 13:15:36 +0000

    vue ui

--- docker-run/xrun.vue.sh
+++ docker-run/xrun.vue.sh
@@ -3,6 +3,7 @@ docker run -it \
   -v /watorvapor/workspace/www.wator.xyz/vue:/opt/vue \
   -v /etc/passwd:/etc/passwd:ro \
   -v /etc/group:/etc/group:ro \
+  -v /watorvapor/workspace/www.wator.xyz/vue/vue:/sbin/vue \
   -v /watorvapor/workspace/www.wator.xyz/vue/home:/home/$USER \
   -u $(id -u $USER):$(id -g $USER) \
   --workdir="/opt/vue" \
