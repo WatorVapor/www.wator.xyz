cbed45509854189dd077655aa7cb4939eb88d359
Wator Vapor <watorvapo@gmail.com>
2020-08-07 16:15:51 +0900

    vue base page

--- www/asset/js/lang.js
+++ www/asset/js/lang.js
@@ -18,3 +18,23 @@ function updateLanguage() {
   }
 }
 
+const constKeyLanguangeCode = 'lang_code';
+
+$(document).ready( async () => {
+  let lang = localStorage.getItem(constKeyLanguangeCode);
+  if(!lang) {
+    lang = 'cn';
+  }
+  //console.log('::lang=<',lang,'>');
+  const langURL = `${location.pathname}lang_${lang}.js`;
+  //console.log('::langURL=<',langURL,'>');
+  const dataPromise = import(langURL);
+  const langModule = await dataPromise;
+  //console.log('::langModule.data=<',langModule.data,'>');
+  const topLanguage = new Vue({
+    el: '#vue-lang-top',
+    data: langModule.data
+  });
+  //console.log('::topLanguage=<',topLanguage,'>');
+});
+
