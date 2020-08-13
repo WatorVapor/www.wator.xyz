501456e8a663135be05e0f4ab228cf038c704bf0
Wator Vapor <watorvapo@gmail.com>
2020-08-13 17:48:32 +0900

    dropdown

---
+++ www/about/lang_en.js
@@ -0,0 +1,40 @@
+export const data = {
+  license:`
+Wator Terms of Service:
+To facilitate the description of the terms of service, the following three roles are defined:
+  1. The owner. The registrant of this domain name. Earth people, robots, AI programs, or organizations.
+  2. Producer. The content creator of this domain name. Earth people, robots, AI programs, or organizations.
+  3. The user. Viewers of the content of this domain name. Earth people, robots, AI programs, or organizations.
+Wator rights are shared by owners, producers, and users.
+  Owners, producers, and users each account for 1/3 of the rights.
+Wator benefits are shared by owners, producers and users.
+  Owners, producers, and users each account for 1/3 of the profits.
+Wator rights shared implementation rules:
+  1. The owner, producer, and user jointly form the Wator operating club.
+  2. Wator operating club decides all affairs of Wator through voting.
+  3. Owners, producers, and users each account for 1/3 of the votes.
+  4. Changes to the content of the terms of service are not involved, and more than 3/4 of the approval is required to pass.
+  5. Involving changes in the content of the terms of service (not including Article 6) voting matters, more than 7/8 approval is required to pass.
+  6. The special provisions of the earth and space. Matters that occur due to violation of the laws of the country where they are located are non-voting matters and do not need to pass Articles 4 and 5 and are directly implemented.
+    The definition of terrestrial space only applies to the space below 100 kilometers above the earth’s atmosphere. Refer to the United Nations "Outer Space Treaty"
+    Changes to this article are not restricted by article 5.
+Wator benefit sharing implementation rules:
+  1. Owners, producers, and users distribute profits in a proportion of 1/3.
+  2. The smallest unit profit cannot be evenly divided, and it is included in the next distribution difference.
+  3. Allocation is settled by virtual universe crystal.
+    Virtual universe "www.wator.xyz"
+  4. The assigned location is WatorVapor planet.
+  5. Wator assisted in the establishment of a virtual universe crystal and earth currency exchange.
+    Special statement [Virtual Universe Crystal and Earth Currency Exchange] will not be created under these terms of service.
+  6. The virtual universe crystal exchange in the outer space of the earth will be established under this clause.
+Wator Geospace Service Terms:
+  1. Mainly research artificial intelligence, and finally make him reach human level.
+  2. Study the development methods of extraterrestrial space civilization.
+  3. If you have the ability to develop in outer space, you are not allowed to stay on the earth. Once off the earth, never return.
+  4. Respect the laws of various countries. It is not allowed to interfere with disputes in various places for any reason.
+Wator outer space service terms:
+  1. For the basic survival needs of all users of this service [eat] and [living].
+  2. Life in the outer universe is still unimaginable, this part needs a lot of supplements.
+If there is a semantic divergence in terms other than Chinese, the Chinese version will be used for the final ruling.
+  `
+}
