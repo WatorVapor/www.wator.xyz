5b2018478ca97e842734ddb4e4f018931c2840e9
Wator Vapor <watorvapo@gmail.com>
2020-08-13 17:44:08 +0900

    dropdown

--- www/layout/navbar.js
+++ www/layout/navbar.js
@@ -1,11 +1,3 @@
-const languageBtn = `
-&lt;div class=&quot;row justify-content-around mt-lg-3 ml-lg-3 mr-lg-3&quot;&gt;&lt;a class=&quot;btn btn-success mr-lg-5&quot; href=&quot;/language?cn&quot; role=&quot;button&quot;&gt;&lt;span class=&quot;flag-icon flag-icon-background flag-icon-cn&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;a class=&quot;btn btn-success &quot; href=&quot;/language?ja&quot; role=&quot;button&quot;&gt;&lt;span class=&quot;flag-icon flag-icon-background flag-icon-jp&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/div&gt;&lt;div class=&quot;row justify-content-around mt-lg-5 ml-lg-3 mr-lg-3 mb-lg-3&quot;&gt;&lt;a class=&quot;btn btn-success mr-lg-5&quot; href=&quot;/language?en&quot; role=&quot;button&quot;&gt;&lt;span class=&quot;flag-icon flag-icon-background flag-icon-us&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;a class=&quot;btn btn-success &quot; href=&quot;/language?ru&quot; role=&quot;button&quot;&gt;&lt;span class=&quot;flag-icon flag-icon-background flag-icon-ru&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/div&gt;
-`;
-
-const applicationBtn = `
-&lt;div class=&quot;row justify-content-around mt-lg-3 ml-lg-4 mr-lg-4&quot;&gt;&lt;a class=&quot;btn btn-lg btn-success mr-lg-5&quot; href=&quot;/starbian&quot; role=&quot;button&quot;&gt;&lt;i class=&quot;material-icons md-48&quot;&gt;device_hub&lt;/i&gt;&lt;/a&gt;&lt;a class=&quot;btn btn-lg btn-success disabled&quot; href=&quot;/scope&quot; role=&quot;button&quot;&gt;&lt;i class=&quot;material-icons md-48&quot;&gt;photo&lt;/i&gt;&lt;/a&gt;&lt;/div&gt;&lt;hr/&gt;&lt;div class=&quot;row justify-content-around mt-lg-3 ml-lg-4 mr-lg-4&quot;&gt;&lt;a class=&quot;btn btn-lg btn-success mr-lg-5 &quot; href=&quot;/wai&quot; role=&quot;button&quot;&gt;&lt;i class=&quot;material-icons md-48&quot;&gt;mic&lt;/i&gt;&lt;/a&gt;&lt;a class=&quot;btn btn-lg btn-success &quot; href=&quot;/story&quot; role=&quot;button&quot;&gt;&lt;i class=&quot;material-icons md-48&quot;&gt;book&lt;/i&gt;&lt;/a&gt;&lt;/div&gt;
-`;
-
 const navbarTemplate = 
 `
   <div class="container-fluid">
@@ -22,17 +14,28 @@ const navbarTemplate =
       </li>
     </ul>
     <ul class="navbar-nav nav justify-content-end">
-      <li class="nav-item active mr-lg-5">
-        <a tabindex="1" href="#" class="btn btn-lg btn-success nav-btn" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" 
-        data-content="${applicationBtn}">
-          <i class="material-icons md-48">apps</i>
-        </a>
-      </li>
-      <li class="nav-item active mr-lg-5">
-        <a tabindex="0" href="#" class="btn btn-lg btn-success nav-btn" data-container="body" data-html="true" data-trigger="focus" data-toggle="popover" data-placement="bottom" 
-        data-content="${languageBtn}">
+      <li class="nav-item dropdown mr-5">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownLang" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="material-icons md-48">language</i>
         </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownLang">
+          <div class="row ml-2">
+            <div class="col">
+              <a class="" href="/language?cn"><span class="flag-icon flag-icon-background flag-icon-cn"></span></a>
+            </div>
+            <div class="col">
+              <a class="" href="/language?ja"><span class="flag-icon flag-icon-background flag-icon-jp"></span></a>
+            </div>
+          </div> 
+          <div class="row ml-2 mt-2">
+            <div class="col">
+              <a class="" href="/language?en"><span class="flag-icon flag-icon-background flag-icon-us"></span></a>
+            </div>
+            <div class="col">
+              <a class="" href="/language?ru"><span class="flag-icon flag-icon-background flag-icon-ru"></span></a>
+            </div>
+          </div>
+        </div>
       </li>
       <li class="nav-item active">
         <a role="button" class="btn btn-warning btn-lg mr-lg-5" href="/secauth/login" role="button">
