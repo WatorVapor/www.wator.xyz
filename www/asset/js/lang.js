c046397b605acc34e1a51f5b9d3b88bdd9f4dce5
Wator Vapor <watorvapo@gmail.com>
2020-08-14 10:29:14 +0900

    sign

--- www/asset/js/lang.js
+++ www/asset/js/lang.js
@@ -1,25 +1,4 @@
-function onClickLanguage(lang) {
-  console.log('lang=<' + lang + '>');
-  if(lang) {
-    localStorage.setItem('operation.lang',lang);
-    updateLanguage();
-  }
-}
-function updateLanguage() {
-  let lang = localStorage.getItem('operation.lang')
-  if(lang && typeof lang === 'string') {
-    let elemLang = document.getElementById("nav.sec.login.lang");
-    console.log('elemLang=<',elemLang,'>');
-    if(elemLang) {
-      elemLang.value = lang;
-      console.log('elemLang.value=<',elemLang.value,'>');
-    }
-    //document.forms['secauth_navi_lang_form'].submit();
-  }
-}
-
 const constKeyLanguangeCode = 'lang_code';
-
 $(document).ready( async () => {
   let lang = localStorage.getItem(constKeyLanguangeCode);
   if(!lang) {
