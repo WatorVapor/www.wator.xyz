c10619b826b7de51e072d4a1ed7c342ef70951ed
Wator Vapor <watorvapo@gmail.com>
2020-08-16 15:00:27 +0900

    websocket

--- wss/edauth.js
+++ wss/edauth.js
@@ -9,20 +9,55 @@ class EDAuth {
   }
   verify(msg) {
     console.log('EDAuth::verify::msg=<',msg,'>');
-    const calcToken = this.calcTokenKey_(msg.auth.key);
-    console.log('EDAuth::verify::calcToken=<',calcToken,'>');
+    if(!msg.token.startsWith('W')) {
+      return false;
+    }
+    const created_at = new Date(msg.ts);
+    const now = new Date();
+    const escape_ms = now - created_at;
+    //console.log('EDAuth::verify::escape_ms=<',escape_ms,'>');
+    if(escape_ms > 1000*5) {
+      console.log('EDAuth::verify::escape_ms=<',escape_ms,'>');
+      return false;
+    } 
+    const calcToken = this.calcTokenKeyBS64_(msg.auth.key);
+    //console.log('EDAuth::verify::calcToken=<',calcToken,'>');
+    if(calcToken !== msg.token) {
+      return false;
+    }
+    const publicKey = nacl.util.decodeBase64(msg.auth.key);
+    const signMsg = nacl.util.decodeBase64(msg.auth.sign);
+    //console.log('EDAuth::verify::publicKey=<',publicKey,'>');
+    //console.log('EDAuth::verify::signMsg=<',signMsg,'>');
+    const orignal = nacl.sign.open(signMsg,publicKey);
+    if(!orignal) {
+      console.log('EDAuth::verify::orignal=<',orignal,'>');
+      return false;
+    }
+    const orignalB64 = nacl.util.encodeBase64(orignal);
+    //console.log('EDAuth::verify::orignalB64=<',orignalB64,'>');
+    const msgCal = Object.assign({},msg);
+    delete msgCal.auth;
+    const toCalSHA3 = CryptoJS.SHA3(JSON.stringify(msgCal), { outputLength: 512  }).toString(CryptoJS.enc.Base64);
+    const toCalRIPEMD160 = CryptoJS.RIPEMD160(toCalSHA3).toString(CryptoJS.enc.Base64);
+    //console.log('EDAuth::verify::toCalRIPEMD160=<',toCalRIPEMD160,'>');
+    if(orignalB64 === toCalRIPEMD160) {
+      return true;
+    } else {
+      console.log('EDAuth::verify::orignalB64=<',orignalB64,'>');
+      console.log('EDAuth::verify::toCalRIPEMD160=<',toCalRIPEMD160,'>');
+    }
     return false;
   }
-  calcTokenKey_ (publicKey) {
-    const bs64Public = nacl.util.encodeBase64(publicKey);
-    //console.log('EDAuth::calcTokenKey_::bs64Public=<',bs64Public,'>');
+  calcTokenKeyBS64_ (bs64Public) {
+    //console.log('EDAuth::calcTokenKeyBS64_::bs64Public=<',bs64Public,'>');
     const hashPublic = CryptoJS.SHA3(bs64Public, { outputLength: 512 }).toString(CryptoJS.enc.Base64);
-    //console.log('EDAuth::calcTokenKey_::hashPublic=<',hashPublic,'>');
+    //console.log('EDAuth::calcTokenKeyBS64_::hashPublic=<',hashPublic,'>');
     const ripemdPublic = CryptoJS.RIPEMD160(hashPublic).toString(CryptoJS.enc.Base64);
-    //console.log('EDAuth::calcTokenKey_::ripemdPublic=<',ripemdPublic,'>');
+    //console.log('EDAuth::calcTokenKeyBS64_::ripemdPublic=<',ripemdPublic,'>');
     const ripeBuffer = nacl.util.decodeBase64(ripemdPublic);
     const tokenKey = Base58.encode(ripeBuffer);
-    //console.log('EDAuth::calcTokenKey_::tokenKey=<',tokenKey,'>');
+    //console.log('EDAuth::calcTokenKeyBS64_::tokenKey=<',tokenKey,'>');
     return tokenKey;
   }
 }
