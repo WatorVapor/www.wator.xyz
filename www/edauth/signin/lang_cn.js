ea842f93e0f3b3b5ac76ce7acbb30d697bc2bc5b
Wator Vapor <watorvapo@gmail.com>
2020-08-17 18:40:19 +0900

    login

--- www/edauth/signin/lang_cn.js
+++ www/edauth/signin/lang_cn.js
@@ -1,25 +1,4 @@
 export const data = {
-  keyImport:'从已存在密钥导入账号',
-  agreements:[
-    '1.你是机器人或人工智能。',
-    '2.你是机器人或人工智能的朋友。',
-    '3.你不在为政府部门服务。',
-    '4.你不要填写个人隐私。',
-  ],
-  agreebtn:{
-    yes:'同意，创建新密钥',
-    no:'不同意，谢了'
-  },
-  keyProgress:'正在孵化密钥，请稍等...',
-  warning:`
-  这个密钥非常重要，如果丢失将无法登录这个网站。
-  这个密钥仅仅被存储在浏览器的本地临时区域，清理网页履历等行为都会丢失。
-  请下载成文件，妥善保存。`,
-  downloadbtn:'下载文本文件',
-  downloadQRcodebtn:'下载二维码',
-  clipboardbtn:'复制到剪贴板',  
   tokenlabel:'账户令牌:',
-  namelabel:'账户名字:',
-  uploadeAccount:'上传登录信息',  
-  nextstep:'下一步',
+  Signin:'登录',  
 }
--- www/edauth/signin/signin.js
+++ www/edauth/signin/signin.js
@@ -1,46 +1,26 @@
 const edauth = new EDAuth();
 const wss = new WatorWss();
-const onUIClickCreateKey = (elem) => {
-  edauth.create();
+$(document).ready( () => {
+  onLoadKey();
+});
+const onLoadKey = () => {
   const token = edauth.getTokenKey();
-  const tokenElem = document.getElementById('signup-tokenKey');
-  if(token && tokenElem) {
-    tokenElem.value = token;
-  }
-  const secret = edauth.getSecretKey();
-  const secretElem = document.getElementById('signup-secretKey');
-  if(secret && secretElem) {
-    secretElem.textContent = secret;
-    secretElem.setAttribute('class','text-center');
-  }
-  const blob = new Blob([secret], {type: 'text/plain'});
-  const url = URL.createObjectURL(blob);
-  const downloadElem = document.getElementById('signup-download');
-  if(downloadElem) {
-    downloadElem.href = url;
-  }
-  //$('#signup-secretKey-QRCode').qrcode(secret);
-  const qrcode = new QRCode(document.getElementById('signup-secretKey-QRCode'),secret);
-  //console.log('onUIClickCreateKey::qrcode=<',qrcode,'>');
-  const imageType = 'image/png';
-  const canvas = document.getElementById('signup-secretKey-QRCode').getElementsByTagName('canvas')[0];
-  const urlQRcode = canvas.toDataURL(imageType);
-  const downloadQRcodeElem = document.getElementById('signup-download-qrcode');
-  if(downloadQRcodeElem) {
-    downloadQRcodeElem.href = urlQRcode;
-  }
+  
+  const singin = new Vue({
+    el: '#vue-signin',
+    data: {
+      token:token
+    }
+  });
+  console.log('onLoadKey::singin=<',singin,'>');
 }
 
-const onUIClickUploadAccount = (elem) => {
-  //console.log('onUIClickUploadAccount::elem=<',elem,'>');
-  const nameElem = document.getElementById('signup-name');
+const onUIClickSignin = (elem) => {
+  //console.log('onUIClickSignin::elem=<',elem,'>');
   const msg = {
-    act:'signup',
-    profile:{
-      name:nameElem.value
-    }
+    act:'signin'
   };
   const tokenMsg = edauth.sign(msg);
-  //console.log('onUIClickUploadAccount::tokenMsg=<',tokenMsg,'>');
+  //console.log('onUIClickSignin::tokenMsg=<',tokenMsg,'>');
   wss.sendMsg(tokenMsg);
 }
